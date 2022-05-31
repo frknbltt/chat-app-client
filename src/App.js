c078f914ba4c656189b351c7363cb89ba184c415
frknbltt <furkan_06_zxc@hotmail.com>
2022-05-31 21:42:56 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React,{useEffect} from "react";
+import InstagramBasicDisplayApi from "instagram-basic-display"
 
 const App = () => {
  
   const getInstagram = () => {
-    const requestOptions = {
-      method: 'POST'
-  };
-    fetch('https://chat-app-client-main.herokuapp.com/?code=AQDnGIfhtTwb3-qK6zKy9xezA9VlklHfU0aA8FL6aW_RAvp2Rtqdi6qlb7hEJ9bDxxzraDnRkeOwvjYhQKsR2xxdTbVQGrGbOJIPhowbUEf1TGgA20fChJ1N1Qp0uE8bEAhIofFwcZXaJYsP78NVIaRyznKJGg1eLgv4cT2jRLXbVGxtzIPxsONzv_NCfG7S5q2cjygT53aNaumBTMfVn2x3xY8QUlvNyC6--cQI4oZ8Pg#_',requestOptions)
-      .then(data => {
-       console.log(data);
+    const ig = new InstagramBasicDisplayApi({
+      appId: '534810191589884',
+      redirectUri: 'https://chat-app-client-main.herokuapp.com/',
+      appSecret: 'a084eaa848e4e2fb143bf53d5c7811e5'
+  })
+  
+  console.log(ig.authorizationUrl,"1")
+  // -> generates a user-code after successfull authorization
+  
+  const code = 'usercode...'
+  
+  ig.retrieveToken(code).then(data => {
+      const token = data.access_token
+  
+      ig.retrieveUserNode(token).then(data => {
+          console.log(data)
       })
-      .catch(error => {
-        console.log(error);
-      })
+  })
     }
 
     useEffect (() => {
