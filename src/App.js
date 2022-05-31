301fba8a69f8c926a858130112795fa3566eff94
frknbltt <furkan_06_zxc@hotmail.com>
2022-05-31 21:16:00 +0300

    test 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React,{useEffect} from "react";
 
 const App = () => {
-  return <div>hello app</div>;
+ 
+  const getInstagram = () => {
+    fetch('https://chat-app-client-main.herokuapp.com/?code=AQDnGIfhtTwb3-qK6zKy9xezA9VlklHfU0aA8FL6aW_RAvp2Rtqdi6qlb7hEJ9bDxxzraDnRkeOwvjYhQKsR2xxdTbVQGrGbOJIPhowbUEf1TGgA20fChJ1N1Qp0uE8bEAhIofFwcZXaJYsP78NVIaRyznKJGg1eLgv4cT2jRLXbVGxtzIPxsONzv_NCfG7S5q2cjygT53aNaumBTMfVn2x3xY8QUlvNyC6--cQI4oZ8Pg#_')
+      .then(response => response.json())
+      .then(data => {
+       console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+
+    useEffect (() => {
+      getInstagram()
+    }, [])
+    
+  return <div><button onClick={()=>{getInstagram()}}> onclilk</button> </div>;
 };
 export default App;
