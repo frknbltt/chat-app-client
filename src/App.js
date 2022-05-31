78e084b6ad3a235f4639161791561059b6ef04dd
frknbltt <furkan_06_zxc@hotmail.com>
2022-05-31 21:20:38 +0300

    ts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ const App = () => {
  
   const getInstagram = () => {
     fetch('https://chat-app-client-main.herokuapp.com/?code=AQDnGIfhtTwb3-qK6zKy9xezA9VlklHfU0aA8FL6aW_RAvp2Rtqdi6qlb7hEJ9bDxxzraDnRkeOwvjYhQKsR2xxdTbVQGrGbOJIPhowbUEf1TGgA20fChJ1N1Qp0uE8bEAhIofFwcZXaJYsP78NVIaRyznKJGg1eLgv4cT2jRLXbVGxtzIPxsONzv_NCfG7S5q2cjygT53aNaumBTMfVn2x3xY8QUlvNyC6--cQI4oZ8Pg#_')
-      .then(response => response.json())
       .then(data => {
        console.log(data);
       })
