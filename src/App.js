e164aff3d310199d170643de251a0f2a6bb7a469
frknbltt <furkan_06_zxc@hotmail.com>
2022-05-31 21:28:08 +0300

    testasd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React,{useEffect} from "react";
 const App = () => {
  
   const getInstagram = () => {
-    fetch('https://chat-app-client-main.herokuapp.com/?code=AQDnGIfhtTwb3-qK6zKy9xezA9VlklHfU0aA8FL6aW_RAvp2Rtqdi6qlb7hEJ9bDxxzraDnRkeOwvjYhQKsR2xxdTbVQGrGbOJIPhowbUEf1TGgA20fChJ1N1Qp0uE8bEAhIofFwcZXaJYsP78NVIaRyznKJGg1eLgv4cT2jRLXbVGxtzIPxsONzv_NCfG7S5q2cjygT53aNaumBTMfVn2x3xY8QUlvNyC6--cQI4oZ8Pg#_')
+    const requestOptions = {
+      method: 'POST'
+  };
+    fetch('https://chat-app-client-main.herokuapp.com/?code=AQDnGIfhtTwb3-qK6zKy9xezA9VlklHfU0aA8FL6aW_RAvp2Rtqdi6qlb7hEJ9bDxxzraDnRkeOwvjYhQKsR2xxdTbVQGrGbOJIPhowbUEf1TGgA20fChJ1N1Qp0uE8bEAhIofFwcZXaJYsP78NVIaRyznKJGg1eLgv4cT2jRLXbVGxtzIPxsONzv_NCfG7S5q2cjygT53aNaumBTMfVn2x3xY8QUlvNyC6--cQI4oZ8Pg#_',requestOptions)
       .then(data => {
        console.log(data);
       })
